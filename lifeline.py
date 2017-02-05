3dd89831b29879be3fa08bb53f981fe9d3f94d70
m.kruglov <m.kruglov@npo-echelon.ru>
2017-02-05 15:12:05 +0300

    Basic methods added

--- lifeline.py
+++ lifeline.py
@@ -1,27 +1,6 @@
 from treelib import Tree
 
 
-# class Tree(object):
-# 	element_id = ''
-# 	message = ''
-# 	type_of_message = 0  # 1 - Сообщение от Тейлора; 2 - [Сообщение системы], 3 - Выбор пользователя
-# 	left = None
-# 	right = None
-
-# 	def __init__(self, message, type):
-# 		pass
-
-# 	def add_message(self, message_text, type=1):
-# 		pass
-# 	def add_reply(self):
-# 		pass
-# 	def __find_same(self):
-# 		pass
-# 	def save_tree(self):
-# 		pass
-# 	def load_tree(self):
-# 		pass
-
 
 # tree = Tree()
 # n1 = tree.create_node('Root')
@@ -31,3 +10,22 @@ from treelib import Tree
 # tree.create_node('Hello', parent=n1.identifier)
 # prev = tree.create_node('Hello', parent=prev.identifier)
 # tree.show(line_type='ascii')
+
+class Message(object):
+	def __init__(self, message_text, message_type):
+		pass
+
+class GameTree(object):
+	def __init__(self, savefile=None):
+		pass
+	def add_message(self, message, message_type):
+		pass
+	def __find_same_message(self, message):
+		pass
+	def add_user_reply(self, message1, message2):
+		pass
+	def save_tree(self, savefile):
+		pass
+	def load_tree(self, savefile):
+		pass
+
